0dde0c6c0e2595feb323213694788451b8edcea9
Ritik841207 <devilraj11011@gmail.com>
2025-10-23 09:39:15 +0530

    first commit

--- index.js
+++ index.js
@@ -39,4 +39,5 @@ app.get("/", (req, res) => {
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
-module.exports = app;
+export default app;
+
